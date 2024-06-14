72e8571935fd810075a6f352cd302036e3c68c6f
SquirrelloDev <olejnike2000@gmail.com>
2024-06-14 16:40:18 +0200

    Add empty dashboard screen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import {StyleSheet} from 'react-native';
+import {QueryClientProvider} from "@tanstack/react-query";
+import {queryClient} from "./utils/api";
+import {NavigationContainer} from "@react-navigation/native";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import Dashboard from "./screens/Dashboard";
+const Stack = createNativeStackNavigator()
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    return (
+        <QueryClientProvider client={queryClient}>
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name={'Dashboard'} component={Dashboard}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+        </QueryClientProvider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
