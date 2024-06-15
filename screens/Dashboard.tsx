d4fb2317f15883222d248dc0ac87cb4a2d9139d7
SquirrelloDev <olejnike2000@gmail.com>
2024-06-15 15:33:52 +0200

    Add Dashboard.tsx screen

--- screens/Dashboard.tsx
+++ screens/Dashboard.tsx
@@ -1,11 +1,10 @@
 import {ActivityIndicator, Text, View} from "react-native";
 import useDrinkListing from "../queries/drink_types/listing";
-
 export default function Dashboard (){
 	const {data, isLoading} = useDrinkListing()
 	return (
-		<View>
-			{!isLoading ? <Text>{data![0].name}</Text> : <ActivityIndicator size={'large'}/> }
+		<View className="items-center bg-red-500">
+			{!isLoading ? <Text className="text-xl">{data!.drinks[0].name}</Text> : <ActivityIndicator size={'large'}/> }
 		</View>
 	)
 }
