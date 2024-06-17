26f6741d17ccd6e4badf657eaa8028228396e261
SquirrelloDev <olejnike2000@gmail.com>
2024-06-17 18:56:40 +0200

    Apply useSafeAreaStyle hook to the Dashboard

--- screens/Dashboard.tsx
+++ screens/Dashboard.tsx
@@ -1,9 +1,15 @@
 import {ActivityIndicator, Text, View} from "react-native";
 import useDrinkListing from "../queries/drink_types/listing";
+import useSafeAreaStyle from "@/hooks/useSafeAreaStyle";
+import {TopBar} from "@/components/Dashboard/TopBar";
+import {RowCalendar} from "@/components/Dashboard/RowCalendar";
 export default function Dashboard (){
 	const {data, isLoading} = useDrinkListing()
+	const insetsStyles = useSafeAreaStyle()
 	return (
-		<View className="items-center bg-red-500">
+		<View style={[insetsStyles]} className="flex-1">
+			<TopBar />
+			<RowCalendar />
 			{!isLoading ? <Text className="text-xl">{data!.drinks[0].name}</Text> : <ActivityIndicator size={'large'}/> }
 		</View>
 	)
