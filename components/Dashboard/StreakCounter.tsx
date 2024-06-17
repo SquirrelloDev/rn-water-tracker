8c5eeea0bf56e0af2e5631808826704e4f006c66
SquirrelloDev <olejnike2000@gmail.com>
2024-06-17 18:54:41 +0200

    Add Top bar component with the streak counter

---
+++ components/Dashboard/StreakCounter.tsx
@@ -0,0 +1,9 @@
+import {Text, View} from "react-native";
+
+export function StreakCounter() {
+	return (
+		<View className='bg-red-500 rounded-full w-10 h-10 justify-center'>
+			<Text className='text-center'>3</Text>
+		</View>
+	)
+}
