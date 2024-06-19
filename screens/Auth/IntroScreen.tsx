c74e8669e80566ad1f965288f6cf9ae27023746b
SquirrelloDev <olejnike2000@gmail.com>
2024-06-19 14:02:48 +0200

    Add IntroCarousel component

--- screens/Auth/IntroScreen.tsx
+++ screens/Auth/IntroScreen.tsx
@@ -1,11 +1,16 @@
 import {View} from "react-native";
 import CustomButton from "@/components/UI/CustomButton";
-
+import {IntroCarousel} from "@/components/Auth/IntroCarousel";
+import useSafeAreaStyle from "@/hooks/useSafeAreaStyle";
+import {styled} from "nativewind";
+const StyledView = styled(View)
 export function IntroScreen({navigation}) {
+	const insets = useSafeAreaStyle()
 	return (
-		<View className='flex-1'>
+		<StyledView style={[insets]} className='flex-1'>
+			<IntroCarousel />
 			<CustomButton title="Zarejestruj się" onPress={() => navigation.navigate('SignUpPersonalData')} />
 			<CustomButton title="Zaloguj się" onPress={() => navigation.navigate('Login')} variant={'outline'}/>
-		</View>
+		</StyledView>
 	)
 }
