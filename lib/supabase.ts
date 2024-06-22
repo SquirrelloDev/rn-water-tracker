492c55bcd716af0bead54c847b89c660f1e67be5
SquirrelloDev <olejnike2000@gmail.com>
2024-06-22 14:53:46 +0200

    Update subabase config file

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,7 +3,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import {AppState} from "react-native";
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL
 const supabaseAnon = process.env.EXPO_PUBLIC_SUPABASE_ANON
-export const supabase = createClient(supabaseUrl, supabaseAnon, {
+export const supabase = createClient(supabaseUrl!, supabaseAnon!, {
     auth: {
         storage: AsyncStorage,
         autoRefreshToken: true,
