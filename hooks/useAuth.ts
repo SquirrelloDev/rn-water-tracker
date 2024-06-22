f0361ba80a808c1f1d4c922c3f1a012759d0efb1
SquirrelloDev <olejnike2000@gmail.com>
2024-06-22 11:59:54 +0200

    Add auth hook and auth queries

---
+++ hooks/useAuth.ts
@@ -0,0 +1,51 @@
+import {AuthParams, SignUpFormType, SignUpPostData, useLoginAuth, useSignOut, useSignupAuth} from "@/queries/auth/auth";
+import {useState} from "react";
+import useAuthStore from "@/stores/authStore";
+import {useNavigation} from "@react-navigation/native";
+import appRoutes from "@/utils/routes";
+import {Alert} from "react-native";
+import {StackNavigation} from "@/types/navigation";
+
+export default function useAuth() {
+    const setIsLoggedIn = useAuthStore(state => state.setIsLoggedIn)
+    const setSession = useAuthStore(state => state.setSession)
+    const navigation = useNavigation<StackNavigation>()
+    const navigateToDashboard = () => {
+        navigation.navigate(appRoutes.dashboard)
+    }
+    const {mutate: signUp, isPending: isSignupPending, isError: isSignupError, error: signUpError} = useSignupAuth((data, variables) => {
+        if(!data.session){
+            Alert.alert('Konto utworzone!','Na skrzynkę przyszedł mail aktywujący konto', [
+                {text: 'OK', isPreferred: true, onPress: () =>  navigation.reset({index: 1, routes: [{name: appRoutes.intro}, {name: appRoutes.login}]})}
+            ])
+        }
+    })
+    const {mutate: logIn, isPending: isLoginPending, isError: isLoginError, error: loginError} = useLoginAuth((data, variables) => {
+        setSession(data.session)
+        setIsLoggedIn(true)
+        navigateToDashboard()
+    })
+    const {mutate: signOut, isPending: isSingOutPending, isError: isSignOutError, error: signOutError} = useSignOut()
+
+    const authStatus = {
+        signUpStatus: {
+            isSignupPending, isSignupError, signUpError
+        },
+        loginStatus: {
+            isLoginPending, isLoginError, loginError
+        },
+        signOutStatus: {
+            isSingOutPending, isSignOutError, signOutError
+        }
+    }
+    const signupHandler = ({email, password, dailyFluidIntake}: SignUpPostData) => {
+        signUp({email, password, dailyFluidIntake})
+    }
+    const loginHandler = ({email, password}: AuthParams) => {
+      logIn({email, password})
+    }
+    const signOutHandler = () => {
+      signOut()
+    }
+    return {authStatus, signupHandler, loginHandler, signOutHandler}
+}
