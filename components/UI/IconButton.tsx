774c9f28352b811e2a8d139bde6b81c47ed84bda
SquirrelloDev <olejnike2000@gmail.com>
2024-06-25 17:35:55 +0200

    Refactor to include import

--- components/UI/IconButton.tsx
+++ components/UI/IconButton.tsx
@@ -1,6 +1,5 @@
-import {Pressable} from "react-native";
 import {Ionicons} from "@expo/vector-icons";
-import {styled} from "nativewind";
+import {StyledPressable} from "@/components/StyledComponents/StyledComponents";
 interface IconButtonProps {
 	icon: keyof typeof Ionicons.glyphMap,
 	color: string,
@@ -8,7 +7,6 @@ interface IconButtonProps {
 	onPress?: () => void
 	classNames?: string
 }
-const StyledPressable = styled(Pressable)
 export function IconButton({icon, size, color, onPress, classNames}:IconButtonProps) {
 	return (
 		<StyledPressable onPress={onPress} className={classNames}>
