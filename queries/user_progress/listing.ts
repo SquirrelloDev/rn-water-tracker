f8c64b27adf5e7254399333452576c75030cf72d
SquirrelloDev <olejnike2000@gmail.com>
2024-06-25 13:01:14 +0200

    Refactor progress query

--- queries/user_progress/listing.ts
+++ queries/user_progress/listing.ts
@@ -1,5 +1,6 @@
 import {supabase} from "@/lib/supabase";
 import {QueryFunction, useQuery} from "@tanstack/react-query";
+import {UserProgressEntry} from "@/types/progress";
 const listprogressQKString = 'List-Progress'
 type ListProgressQK = [typeof listprogressQKString]
 type ListProgressResponse = {
@@ -25,21 +26,24 @@ type ListUserParams = {
 }
 type ListUserQK = [typeof listuserQKString, ListUserParams]
 type UserProgressResponse = {
-    progress: {
-        date: Date,
-        intake: number,
-    }[]
+    progress: UserProgressEntry[]
 }
 const listUserProgress:QueryFunction<UserProgressResponse, ListUserQK> = async ({queryKey}) => {
     const [,{date, userId}] = queryKey
     const {data, error} = await supabase.from('user_progress').select(`
     date,
-    intake
+    intake,
+    time,
+    drink_types (
+        id,
+        name
+    )
     `).eq('user_id', userId).eq('date', date)
     if(error){
         throw new Error(error.message)
     }
-    return {progress: data}
+    const finalData = data?.map(item => ({date: item.date, intake: item.intake, time: item.time, drink: {id: item.drink_types.id, name: item.drink_types.name}}))
+    return {progress: finalData}
 }
 export default function useProgressListing(){
     const {data, isError, isLoading} = useQuery({queryKey: ['List-Progress'], queryFn: listProgress})
