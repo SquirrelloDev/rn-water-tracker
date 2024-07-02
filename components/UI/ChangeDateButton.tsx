d46d07db2807e270fd7d31074b92526830f900cc
SquirrelloDev <olejnike2000@gmail.com>
2024-07-02 14:46:42 +0200

    Add button component for modyfing the date of entries

---
+++ components/UI/ChangeDateButton.tsx
@@ -0,0 +1,33 @@
+import {StyledPressable, StyledText, StyledView} from "@/components/StyledComponents/StyledComponents";
+import {Ionicons} from "@expo/vector-icons";
+import {useEffect, useState} from "react";
+import {getCurrentTimeString} from "@/utils/days";
+
+interface ChangeDateButtonProps {
+    date: string,
+    time?: string
+    onPress: () => void
+}
+
+export function ChangeDateButton({date, time, onPress}: ChangeDateButtonProps) {
+    const [createTime, setCreateTime] = useState<string>(getCurrentTimeString())
+    useEffect(() => {
+        let timer;
+        if(!time){
+            timer = setInterval(() => {
+            setCreateTime(getCurrentTimeString())
+        }, 1000)
+        }
+        return () => {
+            clearInterval(timer)
+        }
+    }, [time])
+    return (
+        <StyledPressable onPress={onPress}>
+            <StyledView className={'flex-row my-4 justify-center items-center'}>
+                <Ionicons name={'pencil'} size={20} color={'rgb(148, 163, 184)'}/>
+                <StyledText className={'ml-2 text-slate-400'}>{date} {time ? time.substring(0,5) : createTime.substring(0,5)}</StyledText>
+            </StyledView>
+        </StyledPressable>
+    )
+}
