e51923acecb7a0f3100d8e4b7fd74152e016681f
Emil Olejnik <main@emilolejnik@gmail.com>
2024-07-22 13:28:35 +0200

    Add tip selection logic

--- components/Stats/TipsSection/TipsContainer.tsx
+++ components/Stats/TipsSection/TipsContainer.tsx
@@ -4,14 +4,19 @@ import {FlatList } from "react-native";
 import tips from "@/constants/tips";
 import {TipDetails} from "@/components/Stats/TipsSection/TipDetails";
 import {useBottomSheetStore} from "@/stores/bottomSheetStore";
+import {useState} from "react";
 
 export function TipsContainer() {
     const sheetType = useBottomSheetStore(state => state.sheetType)
+    const [tipIndex, setTipIndex] = useState<number>(0)
+    const selectTipHandler = (idx: number) => {
+      setTipIndex(idx)
+    }
     return (
         <StyledView className={'flex-row items-center my-2'}>
             <FlatList horizontal showsHorizontalScrollIndicator={false} data={tips} keyExtractor={(item) => item.id}
-                      renderItem={({item}) => <TipCard title={item.title} image={item.imageName} id={item.id}/>}/>
-            {sheetType === 'tipsInfo' && <TipDetails tip={tips[0]}/>}
+                      renderItem={({item, index}) => <TipCard title={item.title} image={item.imageName} idx={index} selectTipHandler={selectTipHandler} id={item.id}/>}/>
+            {sheetType === 'tipsInfo' && <TipDetails tip={tips[tipIndex]}/>}
         </StyledView>
     );
 }
