8275bb02e9de837fa65005b604922f20da324afc
Emil Olejnik <main@emilolejnik@gmail.com>
2024-07-26 16:21:35 +0200

    Add dismissing keyboard on PersonalSettings.tsx

--- components/AccountSettings/PersonalSettings.tsx
+++ components/AccountSettings/PersonalSettings.tsx
@@ -1,14 +1,18 @@
 import {StyledView} from "@/components/StyledComponents/StyledComponents";
 import {Card} from "@/components/UI/Card";
 import {PersonalDataForm} from "@/components/AccountSettings/PersonalDataForm";
+import {Keyboard, TouchableWithoutFeedback} from "react-native";
 
 function PersonalSettings() {
     return (
-        <StyledView className={'flex-1'}>
-            <Card classNames={'px-1'}>
-                <PersonalDataForm />
-            </Card>
-        </StyledView>
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+            <StyledView className={'flex-1'}>
+                <Card classNames={'px-1'}>
+                    <PersonalDataForm/>
+                </Card>
+            </StyledView>
+        </TouchableWithoutFeedback>
     );
 }
+
 export default PersonalSettings
