0de8503dc9f3942c768825b40dc6d8f6f978d9fd
Emil Olejnik <main@emilolejnik@gmail.com>
2024-07-26 13:24:18 +0200

    Add Tab view for account screen

--- screens/Settings/AccountScreen.tsx
+++ screens/Settings/AccountScreen.tsx
@@ -1,9 +1,28 @@
-import {StyledText, StyledView} from "@/components/StyledComponents/StyledComponents";
+import {useWindowDimensions} from "react-native";
+import {SceneMap, TabView} from "react-native-tab-view";
+import {useState} from "react";
+import AccountSettings from "@/components/AccountSettings/AccountSettings";
+import PersonalSettings from "@/components/AccountSettings/PersonalSettings";
+
+const renderScene = SceneMap({
+    accountInformation: AccountSettings,
+    personalInformation: PersonalSettings,
+});
 
 export function AccountScreen() {
+    const layout = useWindowDimensions();
+
+    const [index, setIndex] = useState(0);
+    const [routes] = useState([
+        { key: 'accountInformation', title: 'Dane konta' },
+        { key: 'personalInformation', title: 'Pozostałe dane' },
+    ]);
     return (
-        <StyledView>
-            <StyledText>AccountScreen</StyledText>
-        </StyledView>
+            <TabView
+                navigationState={{ index, routes }}
+                renderScene={renderScene}
+                onIndexChange={setIndex}
+                initialLayout={{ width: layout.width }}
+            />
     );
 }
