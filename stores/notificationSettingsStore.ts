fe8b57459efd4259a01e7531c3f709a69aeb11c8
Emil Olejnik <main@emilolejnik@gmail.com>
2024-07-30 16:06:20 +0200

    Add extra properties to the notification store for sounds; add notification sounds constants

--- stores/notificationSettingsStore.ts
+++ stores/notificationSettingsStore.ts
@@ -1,6 +1,6 @@
 import {create} from "zustand";
 import {createJSONStorage, persist} from "zustand/middleware";
-import intervals from "@/constants/notificationIntervals";
+import intervals, {notificationSounds, Sounds} from "@/constants/notificationIntervals";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export interface NotificationSettingsStore {
@@ -8,11 +8,17 @@ export interface NotificationSettingsStore {
     setNotificationsAllowed: (value: boolean) => void
     waterNotificationInterval: number
     setWaterNotificationInterval: (interval: number) => void
+    soundEnabled: boolean
+    setSoundEnabled: (value: boolean) => void
+    notificationSound: Sounds
+    setNotificationSound: (soundObject: Sounds) => void
 }
 
 const useNotificationSettingsStore = create<NotificationSettingsStore>()(persist((setState) => ({
     notificationsAllowed: false,
     waterNotificationInterval: intervals[0].value,
+    soundEnabled: true,
+    notificationSound: notificationSounds[0],
     setNotificationsAllowed: (value) => {
         setState(() => ({
             notificationsAllowed: value
@@ -22,6 +28,16 @@ const useNotificationSettingsStore = create<NotificationSettingsStore>()(persist
         setState(() => ({
             waterNotificationInterval: interval
         }))
-    }
+    },
+    setNotificationSound: (soundObject) => {
+        setState(() => ({
+            notificationSound: soundObject
+        }))
+    },
+    setSoundEnabled: (value) => {
+        setState(() => ({
+            soundEnabled: value
+        }))
+    },
 }), {name: 'notificationSettings', storage: createJSONStorage(() => AsyncStorage)}))
 export default useNotificationSettingsStore
