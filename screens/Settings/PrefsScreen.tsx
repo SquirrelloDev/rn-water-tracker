f45583258ac216c3cbc0e20829ddd5d89693ca59
Emil Olejnik <main@emilolejnik@gmail.com>
2024-07-30 16:59:26 +0200

    Add temporary button for toggling the theme

--- screens/Settings/PrefsScreen.tsx
+++ screens/Settings/PrefsScreen.tsx
@@ -1,9 +1,14 @@
 import {StyledText, StyledView} from "@/components/StyledComponents/StyledComponents";
+import {Button} from "react-native";
+import React from "react";
+import {useColorScheme} from "nativewind";
 
 export function PrefsScreen() {
+    const {colorScheme, toggleColorScheme} = useColorScheme()
     return (
         <StyledView>
             <StyledText>prefs</StyledText>
+            <Button title={'toggle theme'} onPress={() => toggleColorScheme()}/>
         </StyledView>
     );
 }
