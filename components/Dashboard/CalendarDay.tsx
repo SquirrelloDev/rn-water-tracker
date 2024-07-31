0285185480db5e849f52963213c7f2e829e0c5aa
Emil Olejnik <main@emilolejnik@gmail.com>
2024-07-31 14:04:24 +0200

    Add dark theme for the calendar

--- components/Dashboard/CalendarDay.tsx
+++ components/Dashboard/CalendarDay.tsx
@@ -32,8 +32,8 @@ export function CalendarDay({day, index, snapToDay}:CalendarDayProps) {
 	// }
 
 	return (
-		<StyledPressable onPress={onPressHandler} disabled={isAfter} className={clsx('mx-3 rounded-full w-12 h-12 justify-center', isSameDate ? 'bg-blue-400' : 'border', isAfter && 'border-gray-400')}>
-			<StyledText className={clsx('text-lg text-center', isSameDate && 'text-white font-semibold', isAfter && 'text-gray-400')}>{day.date}</StyledText>
+		<StyledPressable onPress={onPressHandler} disabled={isAfter} className={clsx('mx-3 rounded-full w-12 h-12 justify-center', isSameDate ? 'bg-blue-400' : 'border dark:border-white', isAfter && 'border-gray-400')}>
+			<StyledText className={clsx('text-lg text-center dark:text-white', isSameDate && 'text-white font-semibold', isAfter && 'text-gray-400')}>{day.date}</StyledText>
 		</StyledPressable>
 	)
 }
