6ca524b3c336d1a1e1812964f6eabce1a6227a74
Emil Olejnik <main@emilolejnik@gmail.com>
2024-08-02 17:41:25 +0200

    Add fallback when user has no connection

--- screens/Settings/BugReportScreen.tsx
+++ screens/Settings/BugReportScreen.tsx
@@ -1,10 +1,16 @@
-import {StyledView} from "@/components/StyledComponents/StyledComponents";
+import {StyledText, StyledView} from "@/components/StyledComponents/StyledComponents";
 import WebView from "react-native-webview";
-
+import {useNetInfoInstance} from '@react-native-community/netinfo'
 export function BugReportScreen() {
+    const {netInfo} = useNetInfoInstance()
     return (
         <StyledView className={'flex-1 dark:bg-neutral-900'}>
-            <WebView source={{uri: 'https://github.com/SquirrelloDev/rn-water-tracker/issues'}}/>
+            {netInfo.isConnected ? <WebView source={{uri: 'https://github.com/SquirrelloDev/rn-water-tracker/issues'}}/> : (
+                <StyledView className={'flex-1 justify-center'}>
+                    <StyledText className={'font-bold text-center text-4xl dark:text-white'}>Brak połączenia 😥</StyledText>
+                    <StyledText className={'font-medium text-lg text-center mt-5 dark:text-white'}>Zgłaszanie błędów jest dostępne tylko przy połączeniu z siecią</StyledText>
+                </StyledView>
+            )}
         </StyledView>
     );
 }
