5dd17bc68fd28987ad59fbe55c98bb08aa7f328d
Emil Olejnik <main@emilolejnik@gmail.com>
2024-08-17 15:03:43 +0200

    Styling: add application's perks

--- components/Auth/IntroCarousel.tsx
+++ components/Auth/IntroCarousel.tsx
@@ -1,20 +1,28 @@
-import {View, Text, StyleSheet} from "react-native";
+import {View, Text, StyleSheet, Image} from "react-native";
 import PagerView from "react-native-pager-view";
+import {StyledText, StyledView} from "@/components/StyledComponents/StyledComponents";
+import {SvgXml} from "react-native-svg";
+import {dataSvgXml, notificationSvgXml, progressSvgXml} from "@/utils/svgxml";
 
 export function IntroCarousel() {
 	//TODO: Customize screens
 	return (
 			<PagerView style={styles.container} initialPage={0} overScrollMode={'never'}>
-				<View style={styles.page} key="1">
-					<Text>First page</Text>
-					<Text>Swipe ➡️</Text>
-				</View>
-				<View style={styles.page} key="2">
-					<Text>Second page</Text>
-				</View>
-				<View style={styles.page} key="3">
-					<Text>Third page</Text>
-				</View>
+				<StyledView className={'p-2'} style={styles.page} key="1">
+					<SvgXml xml={progressSvgXml} height='50%' width='80%'/>
+					<StyledText className={'font-bold text-center text-3xl my-2 dark:text-white'}>Śledź swoje postępy</StyledText>
+					<StyledText className={'text-base text-center dark:text-white'}>Drink guard śledzi twoje postępy w spożyciu płynów</StyledText>
+				</StyledView>
+				<StyledView className={'p-2'} style={styles.page} key="2">
+					<SvgXml xml={dataSvgXml} height='50%' width='80%'/>
+					<StyledText className={'font-bold text-center text-3xl my-2 dark:text-white'}>Analizuj swoje nawyki</StyledText>
+					<StyledText className={'text-base text-center dark:text-white'}>Sprawdź, ile płynów udało ci się wypić w różnych okresach czasu</StyledText>
+				</StyledView>
+				<StyledView className={'p-2'} style={styles.page} key="3">
+					<SvgXml xml={notificationSvgXml} height='50%' width='80%'/>
+					<StyledText className={'font-bold text-center text-3xl my-2 dark:text-white'}>Wykorzystaj moc powiadomień</StyledText>
+					<StyledText className={'text-base text-center dark:text-white'}>Zezwól na powiadomienia, by nie zapomnieć o napiciu się, oraz dostosuj je do własnych potrzeb</StyledText>
+				</StyledView>
 			</PagerView>
 	)
 }
